91553849a596b5c2db75f37c01d3dae5f38e55c3
Chris Tickner <chris.tickner@gmail.com>
2014-01-19 11:35:10 -0400

    adds the new extension

---
+++ chrome-ext/content.js
@@ -0,0 +1,88 @@
+console.log('CONTENT SCRIPT LOADED');
+
+function privMessage(user, target) {
+    // var urlParams = window.location.split('/');
+    // console.log(urlParams)
+    //var urlParams = window.location.href.split('/');
+
+
+}
+function setHandlers() {
+    $("body .gi-color").on('click',function(){
+       privMessage(me, el.someExt);
+    });
+}
+function createChat(room) {
+    // Create a new instance of the Chat widget
+    var chat = new goinstant.widgets.Chat({
+        room: room
+    });
+
+    // Initialize the Chat widget
+    chat.initialize(function(err) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        $('.gi-chat').each(function(index){
+            var width = 275;
+            $(this).css('margin-right', width*(index+1));
+        });
+    });
+}
+function createUserList(room,side) {
+    // Create a new instance of the UserList widget
+    var userList = new goinstant.widgets.UserList({
+        room: room,
+        collapsed: false,
+        position: side,
+        userOptions: true,
+        avatars: true
+    });
+
+    // Initialize the UserList widget
+    userList.initialize(function(err) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        // Now it should render on the page with any connected users
+    });
+}
+function connect(service, chan, side) {
+    var url = 'https://goinstant.net/androolloyd/back-seat-gamer';
+    
+    //var urlParams = window.location.split('/');
+    //
+    // Connect to GoInstant
+    goinstant.connect(url, function(err, platformObj, roomObj) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        var serviceRoom = service + "-" + chan;
+        var room = platformObj.room(serviceRoom);
+        // join room and initialize the chat
+        room.join(function(err, yourRoom, userData) {
+            if (err) {
+                console.log("Error joining room:", err);
+                // Failed to join room; clean up or retry.
+                return;
+            }
+            createChat(room);
+            createUserList(room,side);
+        });
+    });
+}
+window.addEventListener("load", function() {
+    connect("twitch","nightblue3","right");
+    connect("twitch","riotgames","left");
+    //connect("twitch","kaostv","left");
+
+});
+
+
+
+
+
+
